1e67c1d43f36d233ea66ffbd3e3b906cc5ef1e96
JubasNTC <jubas1337@mail.ru>
2021-03-25 15:16:29 +0300

    create block model db

---
+++ migrations/20210325080641-create-block.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Blocks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      number: {
+        type: Sequelize.STRING,
+      },
+      hash: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Blocks');
+  },
+};
---
+++ models/block.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Block extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Block.hasMany(models.Transaction, {
+        foreignKey: 'blockId',
+        as: 'transactions',
+      });
+    }
+  }
+  Block.init(
+    {
+      number: DataTypes.STRING,
+      hash: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Block',
+    }
+  );
+  return Block;
+};
