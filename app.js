e25e0f56318505d12b5d3dcc8ae3226ad439266d
JubasNTC <jubas1337@mail.ru>
2021-03-25 15:13:49 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const createError = require('http-errors');
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
